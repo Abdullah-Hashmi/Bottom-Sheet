a72071592ee05339bed7d05e96f1978f42e03d95
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-10-21 16:52:50 +0500

    update Bottomsheet

---
+++ constants/images.ts
@@ -0,0 +1,63 @@
+const user1 = require("../assets/images/user1.jpeg");
+const user2 = require("../assets/images/user2.jpeg");
+const user3 = require("../assets/images/user3.jpeg");
+const user4 = require("../assets/images/user4.jpeg");
+const user5 = require("../assets/images/user5.jpeg");
+const user6 = require("../assets/images/user6.jpeg");
+const user7 = require("../assets/images/user7.jpeg");
+const user8 = require("../assets/images/user8.jpeg");
+const user9 = require("../assets/images/user9.jpeg");
+const user10 = require("../assets/images/user10.jpeg");
+const user11 = require("../assets/images/user11.jpeg");
+const user12 = require("../assets/images/user12.jpg");
+const user13 = require("../assets/images/user13.jpg");
+const user14 = require("../assets/images/user14.jpg");
+const user15 = require("../assets/images/user15.jpg");
+const user16 = require("../assets/images/user16.jpg");
+const user17 = require("../assets/images/user17.jpg");
+const user18 = require("../assets/images/user18.jpg");
+const user19 = require("../assets/images/user19.jpg");
+const user20 = require("../assets/images/user20.jpg");
+const user21 = require("../assets/images/user21.jpg");
+const user22 = require("../assets/images/user22.jpg");
+const user23 = require("../assets/images/user23.jpg");
+const user24 = require("../assets/images/user24.jpg");
+const user25 = require("../assets/images/user25.jpg");
+const user26 = require("../assets/images/user26.jpg");
+const user27 = require("../assets/images/user27.jpg");
+const user28 = require("../assets/images/user28.jpg");
+const user29 = require("../assets/images/user29.jpg");
+const user30 = require("../assets/images/user30.jpg");
+
+export default {
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+  user10,
+  user11,
+  user12,
+  user13,
+  user14,
+  user15,
+  user16,
+  user17,
+  user18,
+  user19,
+  user20,
+  user21,
+  user22,
+  user23,
+  user24,
+  user25,
+  user26,
+  user27,
+  user28,
+  user29,
+  user30,
+};
